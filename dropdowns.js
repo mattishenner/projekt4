d9c9e23b4451c57a6c596717716cfc90929655f4
Stikkelmaal <denlacour@gmail.com>
2023-04-13 12:17:07 +0200

    arrow animation :')

---
+++ dropdowns.js
@@ -0,0 +1,27 @@
+// const dropDownArrow = document.querySelectorAll('.down-arrow');
+// console.log(dropDownArrow);
+
+// function rotateArrow(){
+//     dropDownArrow.classList.add('dropdownrotate');
+//     document.addEventListener('click', function(event){
+//         let isArrowClicked = dropDownArrow.contains(event.target);
+//         if (!isArrowClicked){
+//             dropDownArrow.classList.remove('dropdownrotate');
+//         }
+//     });
+// }
+
+const dropDownArrow = document.querySelectorAll('.down-arrow');
+
+function rotateArrow(index) {
+  dropDownArrow.forEach((arrows) => {
+    arrows.classList.remove('dropdownrotate');
+  });
+  dropDownArrow[index].classList.add('dropdownrotate');
+  document.addEventListener('click', function(event){
+    let isArrowClicked = dropDownArrow[index].contains(event.target);
+    if (!isArrowClicked){
+      dropDownArrow[index].classList.remove('dropdownrotate');
+    }
+  });
+}
