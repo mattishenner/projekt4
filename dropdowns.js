7339ea727df1e3681b4c83cfec56db32e0d0e486
mattishenner <118067835+mattishenner@users.noreply.github.com>
2023-04-17 16:52:21 +0200

    Fungerende dropdown menuer

--- dropdowns.js
+++ dropdowns.js
@@ -11,23 +11,43 @@
 //     });
 // }
 
-const dropDownArrow = document.querySelectorAll('.down-arrow');
-const dropDownContents = document.querySelector('.dropdown-contents');
+// const dropDownArrow = document.querySelectorAll('.down-arrow');
+// const dropDownContents = document.querySelector('.dropdown-contents');
 
 
-function rotateArrow(index) {
-  dropDownArrow.forEach((arrows) => {
-    arrows.classList.remove('dropdownrotate');
+// function rotateArrow(index) {
+//   dropDownArrow.forEach((arrows) => {
+//     arrows.classList.remove('dropdownrotate');
+//   });
+//   dropDownArrow[index].classList.add('dropdownrotate');
+//   dropDownContents.classList.add('showdropdown');
+//   document.addEventListener('click', function(event){
+//     let isArrowClicked = dropDownArrow[index].contains(event.target);
+//     let isMenuClicked = dropDownContents.contains(event.target);
+//     if (!isArrowClicked && !isMenuClicked){
+//       dropDownArrow[index].classList.remove('dropdownrotate');
+//       dropDownContents.classList.remove('showdropdown');
+//     }
+//   });
+// }
+
+
+const dropdowns = document.querySelectorAll('.dropdown-wrapper');
+
+dropdowns.forEach((dd) => {
+  const links = dd.querySelectorAll('.dropdown-list a');
+  const arrow = dd.querySelector(".down-arrow")
+  const span = dd.querySelector('span');
+  
+  dd.addEventListener('click', function() {
+    this.classList.toggle('is-active');
+    arrow.classList.toggle("is-active");
   });
-  dropDownArrow[index].classList.add('dropdownrotate');
-  dropDownContents.classList.add('showdropdown');
-  document.addEventListener('click', function(event){
-    let isArrowClicked = dropDownArrow[index].contains(event.target);
-    let isMenuClicked = dropDownContents.contains(event.target);
-    if (!isArrowClicked && !isMenuClicked){
-      dropDownArrow[index].classList.remove('dropdownrotate');
-      dropDownContents.classList.remove('showdropdown');
-    }
-  });
-}
 
+
+  links.forEach((element) => {
+    element.addEventListener('click', function(evt) {
+      span.innerHTML = evt.currentTarget.textContent;
+    })
+  })
+})
