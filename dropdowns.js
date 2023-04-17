faf3b2436434774d7c7c08b795cc708ef91b65f5
mattishenner <118067835+mattishenner@users.noreply.github.com>
2023-04-17 17:42:43 +0200

    Animation plus data-values

--- dropdowns.js
+++ dropdowns.js
@@ -33,6 +33,7 @@
 
 
 const dropdowns = document.querySelectorAll('.dropdown-wrapper');
+let animationLength;
 
 dropdowns.forEach((dd) => {
   const links = dd.querySelectorAll('.dropdown-list a');
@@ -48,6 +49,38 @@ dropdowns.forEach((dd) => {
   links.forEach((element) => {
     element.addEventListener('click', function(evt) {
       span.innerHTML = evt.currentTarget.textContent;
+      animationLength = element.dataset.value;
+      console.log(animationLength);
     })
   })
+})
+
+
+// Lottie animation
+let player = document.querySelector("lottie-player");
+let playBtn = document.querySelector(".play-animation");
+let isPlaying = false;
+
+playBtn.addEventListener("click", () => {
+  if (isPlaying === false){
+    player.play();
+    console.log("start")
+    playBtn.textContent = "Stop";
+    isPlaying = true;
+    timeoutId = setTimeout(() => {
+      player.pause();
+      playBtn.textContent = "Start";
+      isPlaying = false;
+      timeoutId = null;
+    }, animationLength * 1000 * 60); // Gang dataen med 1000 for at gå fra ms til sekunder. Gang med 60 for at gå til minutter.
+
+  } else if (isPlaying === true) {
+    player.pause();
+    console.log("stop");
+    playBtn.textContent = "Start";
+    isPlaying = false;
+    clearTimeout(timeoutId);
+    timeoutId = null;
+    
+  }
 })
