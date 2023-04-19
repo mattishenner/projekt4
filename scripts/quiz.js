e9c2e912fe9392cf32cca0e4a1f0c59bade9b2c5
simonostergaard <iversen96c@gmail.com>
2023-04-19 11:12:14 +0200

    slette teskt

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,4 +1,4 @@
-//Her laves spørgsmålene til quizen + svarmulighederne
+
 const questions = [
     {
         question: "I nogle tilfælde kan stress, hjælpe en i en presset situation som for eksempel en eksamen, fordi:?",
@@ -47,26 +47,21 @@ const questions = [
     }
 ];
 
-//Tilføjelse af variablerne for 3 elementer (question, answer-btn, next-btn)
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 const continueButton = document.getElementById("continue-btn");
 
-//Variabler for at holde styr på spørgsmåls index og score
 let currentQuestionIndex = 0;
 let score = 0;
 
-//Funktion til at starte quiz - her vil spørgsmålet reset index og score til 0
 function startQuiz(){
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Næste";
     showQuestion();
-    // resetAnimation();
 }
 
-//Denne funktion vil vise spørgsmålet med nummer og svarmulighederne der hører til 
 function showQuestion(){
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
@@ -74,22 +69,17 @@ function showQuestion(){
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
     
     currentQuestion.answers.forEach(answer => {
-        // Her tilføjer vi svarene
         const button = document.createElement("button");
-        // Her bliver teksten tilføjet til btn
         button.innerHTML = answer.text;
         button.classList.add("answer-btn");
-        // for at vise button i div'en (id="answer-buttons") 
         answerButtons.appendChild(button);
         if(answer.correct){
             button.dataset.correct = answer.correct;
         }
-        //Når man klikker vil tage fat i denne funktion
         button.addEventListener("click", selectAnswer);
     })
 }
 
-//Fjerner alle tidlige svar
 function resetState(){
    nextButton.style.display = "none";
    while(answerButtons.firstChild){
@@ -98,9 +88,7 @@ function resetState(){
 }
 
 function selectAnswer(e){
-    // efter klik vil den tage dette element i variablen
     const selectedBtn = e.target;
-    // så vil den tjekke data set og se om den er true eller ikke add class 
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
@@ -108,15 +96,12 @@ function selectAnswer(e){
     }else{
         selectedBtn.classList.add("incorrect");
     }
-    //array for answer buttons children
     Array.from(answerButtons.children).forEach(button => {
         if(button.dataset.correct === "true"){
             button.classList.add("correct");
         }
-        // disabled klik så svaret er låst
         button.disabled = true;
     });
-    //Herefter vil next btn vis sig
     nextButton.style.display = "block";
 }
 
@@ -128,7 +113,6 @@ function showScore(){
     playAnimation();
 };
 
-// Vil vise spørgsmål eller score, hvis der ikke er flere spørgsmål
 function handleNextButton(){
     currentQuestionIndex++;
     if(currentQuestionIndex < questions.length){
@@ -138,18 +122,23 @@ function handleNextButton(){
     }
 };
 
-// Funktion for next-btn 
 nextButton.addEventListener("click", ()=>{
-    // vil se hvor mange spørgsmål der har været 
     if(currentQuestionIndex < questions.length){
         handleNextButton();
-    // hvis flere end 4 vil den restarte quiz
     }else{
         startQuiz();
     }
 });
 
 startQuiz();
+
+
+
+
+
+
+
+
 
 // *************** Confetti Animation *************** //
 let animation = bodymovin.loadAnimation({
